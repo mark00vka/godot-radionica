254e2d997df83eb1dfb943a431320a25d80d32c3
markovka <mark.mozh@gmail.com>
2026-03-24 13:34:24 +0100

    add spawn animation

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,6 +2,12 @@ extends RigidBody2D
 
 @export var ball_velocity : float = 500.0
 
+func _ready() -> void:
+	set_deferred("freeze", true)
+	await get_tree().create_timer(1).timeout
+	set_deferred("freeze", false)
+	linear_velocity = Vector2(1, -1)
+
 func _physics_process(_delta: float) -> void:
 	var direction = linear_velocity.normalized()
 	linear_velocity = direction * ball_velocity
--- scripts/block.gd
+++ scripts/block.gd
@@ -1,4 +1,18 @@
 class_name Block
 extends StaticBody2D
+
+func _ready() -> void:
+	play_spawn_animation()
+	
+func play_spawn_animation():
+	var end_pos := global_position
+	global_position += Vector2.UP * 400.0
+	scale = Vector2(0.1, 0.1)
+	await get_tree().create_timer(randf()/4.0).timeout
+	
+	var tween = create_tween()
+	tween.tween_property(self, 'global_position', end_pos, 0.5).set_trans(Tween.TRANS_CUBIC).set_delay(randf()/2.0)
+	tween.parallel().tween_property(self, 'scale', Vector2(1,1), 0.5).set_trans(Tween.TRANS_LINEAR).set_delay(0.2)
+
 func destroy():
 	queue_free()
