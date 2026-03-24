dfc8955625453327e709c94d29d9fef7fe575700
markovka <mark.mozh@gmail.com>
2026-03-24 13:51:09 +0100

    add destroy block animation

--- scripts/block.gd
+++ scripts/block.gd
@@ -15,4 +15,21 @@ func play_spawn_animation():
 	tween.parallel().tween_property(self, 'scale', Vector2(1,1), 0.5).set_trans(Tween.TRANS_LINEAR).set_delay(0.2)
 
 func destroy():
+	$CollisionShape2D.disabled = true
+	play_destroy_animation()
+func play_destroy_animation():
+	var direction = randf_range(-1, 1)
+	var end_pos = global_position + Vector2.DOWN * 200 + Vector2.RIGHT * direction * 100
+	var end_rot = global_rotation + 4 * PI * direction
+	
+	var tween = create_tween()
+	tween.tween_property(self, 'global_position:x', end_pos.x, 0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	# tween.set_parallel()
+	tween.parallel().tween_property(self, 'global_position:y', end_pos.y, 0.5).set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property(self, 'scale', Vector2(0,0), 0.5).set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property(self, 'global_rotation', end_rot, 0.5)
+	tween.parallel().tween_property(self, 'modulate:a', 0, 0.5)
+	
+	# tween.tween_callback(queue_free) (isto kao ovo dole)
+	await tween.finished
 	queue_free()
